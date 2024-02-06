e2b2b31d151c3fdd8610600661b99d075ace6075
vlad333rrty <vlad333rrty@yandex-team.ru>
2024-02-06 14:28:47 +0300

    wip

--- temp_infer_client.py
+++ temp_infer_client.py
@@ -5,6 +5,7 @@ import torch
 from src.taxo_expantion_methods.TEMP.client.temp_infer import infer_many_async
 from src.taxo_expantion_methods.TEMP.temp_model import TEMP
 from src.taxo_expantion_methods.common import performance
+from src.taxo_expantion_methods.common.Term import Term
 from src.taxo_expantion_methods.common.wn_dao import WordNetDao
 
 args = argparse.ArgumentParser(description='Training taxonomy expansion model')
@@ -16,17 +17,18 @@ args.add_argument('-res', '--result-path', default=None, type=str, help='Result 
 args = args.parse_args()
 
 
-def read_terms(path):
+def read_terms(path, limit):
     with open(path, 'r') as file:
-        return file.readlines()
+        res = []
+        for i in range(limit):
+            res.append(file.readline().strip())
+        return res
 
 
 wn_reader = WordNetDao.get_wn_30()
-terms = read_terms(args.terms_path)
+terms = read_terms(args.terms_path, args.limit)
 
-if args.limit is not None:
-    terms = terms[:args.limit]
-
+terms = list(map(lambda x: Term(x, wn_reader.synsets(x)[0].definition()), terms))
 model = TEMP(768 * 2, 256).to(args.device)
 model.load_state_dict(torch.load(args.load_path, map_location=torch.device(args.device)))
 delta, results = performance.measure(lambda: infer_many_async(model, terms, args.device, 4))
