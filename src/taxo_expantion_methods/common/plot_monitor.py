899b8112ffdc18b228e7467694d0a952bfff41c8
vlad333rrty <vlad333rrty@yandex-team.ru>
2024-05-03 01:46:25 +0300

    wip

--- src/taxo_expantion_methods/common/plot_monitor.py
+++ src/taxo_expantion_methods/common/plot_monitor.py
@@ -13,14 +13,18 @@ class Metric:
 class PlotMonitor:
     def __init__(self):
         self.__buffers = {}
+        self.__sma_by_label = {}
         self.__lock = threading.Lock()
 
     def accept(self, metric: Metric):
         self.__lock.acquire()
-
-        if metric.label not in self.__buffers:
-            self.__buffers[metric.label] = []
-        self.__buffers[metric.label].append(metric.value)
+        label = metric.label
+        if label not in self.__buffers:
+            self.__buffers[label] = []
+            self.__sma_by_label[label] = []
+        self.__buffers[label].append(metric.value)
+        sma = self.__get_sma(label)
+        self.__sma_by_label[label].append(sma)
 
         self.__lock.release()
 
@@ -31,15 +35,23 @@ class PlotMonitor:
         plt.show()
 
         for label in self.__buffers:
-            plt.plot(self.__buffers[label], label=label)
+            plt.plot(self.__sma_by_label[label], label=label)
 
         plt.legend()
         plt.show()
         plt.ioff()
         self.__lock.release()
 
+    def __get_sma(self, label, n=7):
+        accum = 0
+        values = self.__buffers[label]
+        bound = min(len(values), n)
+        for i in range(0, bound):
+            accum += values[-i]
+        return accum / bound
 
     def clear(self):
         self.__lock.acquire()
         self.__buffers.clear()
+        self.__sma_by_label.clear()
         self.__lock.release()
