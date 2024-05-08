b97ef8ec42ce39959ca07be011dd814626aa96be
vlad333rrty <vlad333rrty@yandex-team.ru>
2024-05-08 18:26:35 +0300

    dropout + batch normalization

--- src/taxo_expantion_methods/TEMP/temp_model.py
+++ src/taxo_expantion_methods/TEMP/temp_model.py
@@ -7,7 +7,9 @@ class TEMP(nn.Module):
         super(TEMP, self).__init__()
         self.model = nn.Sequential(
             nn.Linear(in_dim, hidden_dim),
+            nn.Dropout(0.2),
             nn.LeakyReLU(0.2),
+            nn.BatchNorm1d(hidden_dim),
             nn.Linear(hidden_dim, 1),
             nn.Sigmoid()
         )
