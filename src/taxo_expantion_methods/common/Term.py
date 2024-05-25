d7968c14945b0fe24c2b520b663bd514c6677b45
vlad333rrty <vlad333rrty@yandex-team.ru>
2024-05-25 16:24:53 +0300

    wip

--- src/taxo_expantion_methods/common/Term.py
+++ src/taxo_expantion_methods/common/Term.py
@@ -1,13 +1,17 @@
 class Term:
-    def __init__(self, value, definition):
+    def __init__(self, value, definition, pos=None):
         self.value = value
         self.definition = definition
+        self.pos = pos
 
     def definition(self):
         return self.definition
 
     def value(self):
         return self.value
+
+    def part_of_speech(self):
+        return self.pos
 
     def __str__(self):
         return self.value
