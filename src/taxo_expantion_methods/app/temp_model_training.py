4e9ed2f0880752926250e2d25812b53fea7420dc
vlad333rrty <vlad333rrty@yandex-team.ru>
2024-05-26 01:13:21 +0300

    wip

--- src/taxo_expantion_methods/app/temp_model_training.py
+++ src/taxo_expantion_methods/app/temp_model_training.py
@@ -1,6 +1,7 @@
 import random
 
 import torch
+from nltk.corpus import WordNetCorpusReader
 from sklearn.model_selection import train_test_split
 from transformers import BertTokenizer, BertModel
 
@@ -14,13 +15,13 @@ from src.taxo_expantion_methods.common.SynsetWrapper import RuSynsetWrapper
 from src.taxo_expantion_methods.common.ru_wn_dao import RuWordnetDao
 
 
-def run_temp_model_training(device, epochs, res_path, model, wn_reader, batch_size=32):
+def run_temp_model_training(device, epochs, res_path, model, wn_reader: WordNetCorpusReader, batch_size=32):
     model = model.to(device)
     optimizer = torch.optim.Adam(model.parameters(), lr=2e-5, betas=(0.9, 0.999))
     loss_fn = TEMPLoss(0.2).to(device)
-    all_synsets = SynsetsProvider.get_all_leaf_synsets_with_common_root(wn_reader.synset('entity.n.01'))
+    all_synsets = wn_reader.all_synsets('n')
 
-    train_synsets, validation_synsets = train_test_split(all_synsets, train_size=0.75, test_size=0.05)
+    train_synsets, validation_synsets = train_test_split(all_synsets, train_size=0.9, test_size=0.05)
 
     tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
     bert_model = BertModel.from_pretrained('bert-base-uncased').to(device)
