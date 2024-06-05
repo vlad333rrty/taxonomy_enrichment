8ce3964ef3b6091a288d47697f3fc4b50b73ef36
vlad333rrty <vlad333rrty@yandex-team.ru>
2024-06-05 09:50:27 +0300

    wip

--- taxo_prompt_train.py
+++ taxo_prompt_train.py
@@ -1,5 +1,13 @@
 from src.taxo_expantion_methods.TaxoPrompt.client.taxo_prompt_client import run
 
-device = 'cpu'
-epochs=4
-run(device, epochs, 0.7, 'data/datasets/taxo-prompt/nouns/ds', 'data/models/TaxoPrompt/checkpoints/taxo_prompt_model_epoch_0')
+import argparse
+
+
+args = argparse.ArgumentParser(description='Training taxonomy expansion model')
+args.add_argument('-d', '--device', default='cpu', type=str, help='used device')
+args.add_argument('-e', '--epochs', default=1, type=int, help='number of epochs')
+args.add_argument('-l', '--load-path', default='data/models/TEMP/pre-trained/temp_model_epoch_9', type=str, help='path to saved model')
+args.add_argument('-res', '--result-path', default='data/models/TEMP/checkpoints', type=str)
+args = args.parse_args()
+
+run(args.device, args.epochs, 0.8, 'data/datasets/taxo-prompt/nouns/ds_food', args.load_path, args.result_path)
