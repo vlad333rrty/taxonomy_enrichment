10f2d6e94c77f783a472ddba254f740a8d2524a0
vlad333rrty <vlad333rrty@yandex-team.ru>
2024-06-05 11:07:40 +0300

    wip

--- src/taxo_expantion_methods/TaxoPrompt/trainer.py
+++ src/taxo_expantion_methods/TaxoPrompt/trainer.py
@@ -57,8 +57,6 @@ class TaxoPromptTrainer:
             tokens = inputs_prompts['input_ids'].to(device)
             attention = inputs_prompts['attention_mask'].to(device)
 
-            expected_tokens = inputs_pdefs['input_ids'].to(device)
-            expected_attention = inputs_pdefs['attention_mask'].to(device)
 
             with torch.no_grad():
                 outputs = self.__bert(
@@ -66,14 +64,10 @@ class TaxoPromptTrainer:
                     output_hidden_states=True,
                     attention_mask=attention
                 )
-                outputs_expected = self.__bert(
-                    expected_tokens,
-                    output_hidden_states=True,
-                    attention_mask=expected_attention
-                )
+            expected_tokens = inputs_pdefs['input_ids'].to(device)
+            labels = torch.where(tokens == self.__tokenizer.mask_token_id, expected_tokens, -100)
             x = self.__model(outputs[0])
-            y = self.__model(outputs_expected[0])
-            loss = self.__loss(x, y)
+            loss = self.__loss(x, labels)
             loss.backward()
             self.__optimizer.step()
 
