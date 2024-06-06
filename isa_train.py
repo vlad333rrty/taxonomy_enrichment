25a8a53732cd9c48a441e11163c6ee9455a1bc00
vlad333rrty <v.murizhnikov@vk.team>
2024-06-07 00:51:12 +0300

    wip

--- isa_train.py
+++ isa_train.py
@@ -14,18 +14,19 @@ args = argparse.ArgumentParser(description='Training taxonomy expansion model')
 args.add_argument('-d', '--device', default='cpu', type=str, help='used device')
 args.add_argument('-e', '--epochs', default=10, type=int, help='number of epochs')
 args.add_argument('-l', '--load-path', default=None, type=str, help='path to saved model')
-# args.add_argument('-res', '--result-path', required=True, type=str)
+args.add_argument('-res', '--result-path', required=True, type=str)
 args.add_argument('-bs', '--batch-size', default=32, type=int)
-# args.add_argument('-ep', '--embeddings-path', required=True, type=str)
+args.add_argument('-ep', '--embeddings-path', required=True, type=str)
 args = args.parse_args()
 
 model = IsAClassifier()
-# if args.load_path is not None:
-model.load_state_dict(torch.load('data/models/isa/temp/isa_model_epoch_9', map_location=torch.device(args.device)))
+if args.load_path is not None:
+    model.load_state_dict(torch.load(args.load_path, map_location=torch.device(args.device)))
 
 wn_reader = WordNetDao.get_wn_30()
 start = time.time()
-embeddings_graph = GraphLoader.load_graph('data/embeddings_graph_no_sum.pkl')
+with open(args.embeddings_path, 'rb') as file:
+    embeddings_graph = pickle.load(file)
 end = time.time()
 print('Loaded embeddings in', end - start, 'sec')
-run_isa_model_training(embeddings_graph, args.device, args.epochs, 'data/models/isa/temp', model, wn_reader, args.batch_size)
+run_isa_model_training(embeddings_graph, args.device, args.epochs, args.result_path, model, wn_reader, args.batch_size)
